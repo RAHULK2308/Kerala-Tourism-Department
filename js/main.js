ff6ca9408fad15723107e69d83319de5aee7a91b
RAHULK2308 <rahulkm4002@gmail.com>
2021-04-20 12:45:36 +0530

    main

--- js/main.js
+++ js/main.js
@@ -4,42 +4,7 @@
         let password=document.getElementById("pwd");
         let email=document.getElementById("Email");
         let error1=document.getElementById("error1");
-        let error2=document.getElementById("error2")
-        
-       function validate(){ 
-            if(email.value.trim()=="" && password.value.trim()==""){
-                text.setAttribute("hidden","true");
-                error1.removeAttribute("hidden","true");
-                error1.style.color="white";
-                email.style.border="1px solid red";
-                error2.removeAttribute("hidden","true");
-                error2.style.color="white";
-                password.style.border="1px solid red";
-              return false;
-            }else if(email.value.trim()==""){
-              error1.removeAttribute("hidden","true");
-                error1.style.color="white";
-                email.style.border="1px solid red";
-                error2.setAttribute("hidden","false");
-                password.style.border="1px solid black";
-              return false;
-            }else if(password.value.trim()==""){
-              text.setAttribute("hidden","true");
-              error2.removeAttribute("hidden","true");
-                error2.style.color="white";
-                password.style.border="1px solid red";
-                error1.setAttribute("hidden","false");
-                email.style.border="1px solid black";
-                return false;
-              }else{
-                error1.setAttribute("hidden","false");
-                error2.setAttribute("hidden","false");
-                email.style.border="1px solid black";
-                password.style.border="1px solid black";
-                return true;
-              }
-      };
-
+        let error2=document.getElementById("error2");
   
       let regexp=/^([\w\d\.-]+)@([\w\d\.-]+)\.([a-z]{2,3})(.[a-z]{z,3})?$/
       function emailvalidate(){
@@ -64,24 +29,24 @@
  
 
       function pwdlength(){
-        if(password.value.trim().length>=8){
-          error2.removeAttribute("hidden","true");
-          error2.innerText="valid"
-          error2.style.color="white";
-          error2.style.backgroundColor="green"
-          password.style.border="1px solid black";
-          return true;
-        }else{
-      
-
+        if(password.value.trim().length<=8){
           error2.removeAttribute("hidden","true");
           error2.innerText="inavlid password"
           error2.style.backgroundColor="red"
           error2.style.color="white";
           password.style.border="1px solid black";
           return false;
+         
+        }else{
+           
+          error2.removeAttribute("hidden","true");
+          error2.innerText="valid"
+          error2.style.color="white";
+          error2.style.backgroundColor="green"
+          password.style.border="1px solid black";
+          return true;
         }
-      }
+      };
 
 
     
